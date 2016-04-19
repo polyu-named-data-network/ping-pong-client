bc2e0dd23ba9eea86437c9538e84b6350736bd31
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-04-19 18:59:41 +0800

    updated data packet and interest packet usage

--- ping-pong-client.go
+++ ping-pong-client.go
@@ -5,14 +5,40 @@ import (
   "bitbucket.org/polyu-named-data-network/ndn/packet/contentname"
   "encoding/json"
   "fmt"
+  "github.com/aabbcc1241/goutils/log"
   "net"
+  "strconv"
   "sync"
 )
 
+func init() {
+  log.Init(true, true, true, log.ShortCommFlag)
+}
 func main() {
   fmt.Println("NDN application demo - ping-pong client start")
 
   wg := sync.WaitGroup{}
+
+  /* establish data connection */
+  dataConn, err := net.Dial("tcp", "127.0.0.1:8124")
+  if err != nil {
+    fmt.Println("failed to connect to proxy data service", err)
+    panic(3)
+  }
+  _, dataPort_string, _ := net.SplitHostPort(dataConn.LocalAddr().String())
+  dataPort, _ := strconv.Atoi(dataPort_string)
+  wg.Add(1)
+  go func() {
+    defer wg.Done()
+    var in_packet packet.DataPacket_s
+    log.Debug.Println("data connection", dataConn.LocalAddr())
+    fmt.Println("wait for data packet")
+    json.NewDecoder(dataConn).Decode(&in_packet)
+    fmt.Println("received data packet", in_packet)
+    fmt.Println("data content (string)", string(in_packet.ContentData))
+  }()
+
+  /* wait for data packet (response)*/
 
   /* establish interest connection */
   interestConn, err := net.Dial("tcp", "127.0.0.1:8123")
@@ -29,6 +55,7 @@ func main() {
     },
     SeqNum:     1,
     AllowCache: false,
+    DataPort:   dataPort,
   }
   err = json.NewEncoder(interestConn).Encode(out_packet)
   if err != nil {
@@ -44,27 +71,10 @@ func main() {
     var in_packet packet.InterestReturnPacket_s
     fmt.Println("wait for interestReturn packet")
     json.NewDecoder(interestConn).Decode(&in_packet)
-    fmt.Println("received interestReturn pcaket", in_packet)
+    fmt.Println("received interestReturn packet", in_packet)
   }()
 
   /* wait for interest return (NAK) */
-
-  /* establish data connection */
-  dataConn, err := net.Dial("tcp", "127.0.0.1:8124")
-  if err != nil {
-    fmt.Println("failed to connect to proxy data service", err)
-    panic(3)
-  }
-  wg.Add(1)
-  go func() {
-    defer wg.Done()
-    var in_packet packet.DataPacket_s
-    fmt.Println("wait for data packet")
-    json.NewDecoder(dataConn).Decode(&in_packet)
-    fmt.Println("received data packet", in_packet)
-  }()
-
-  /* wait for data packet (response)*/
 
   wg.Wait()
   fmt.Println("NDN application demo - ping-pong client end")
